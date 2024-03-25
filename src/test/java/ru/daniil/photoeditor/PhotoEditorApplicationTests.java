30c3d5c04d43c3a9f7afa3de317853f24b45070c
Danii <Daniil141980@yandex.ru>
2024-03-26 00:11:03 +0300

    initial commit

---
+++ src/test/java/ru/daniil/photoeditor/PhotoEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daniil.photoeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
