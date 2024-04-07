527bcb930478a4abd6d9122271ae432c76417066
Danii <Daniil141980@yandex.ru>
2024-04-07 21:46:29 +0300

    feat: add integration test

---
+++ src/test/java/ru/daniil/photoeditor/integration/PhotoEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daniil.photoeditor.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.daniil.photoeditor.config.AbstractBaseTest;
+
+@SpringBootTest
+public class PhotoEditorApplicationTests extends AbstractBaseTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
