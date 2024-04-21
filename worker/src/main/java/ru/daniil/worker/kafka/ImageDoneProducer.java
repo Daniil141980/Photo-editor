4d7d42330df27f00053ba7e21a49603686cd8978
Danii <Daniil141980@yandex.ru>
2024-04-21 17:30:01 +0300

    feat:add module worker

---
+++ worker/src/main/java/ru/daniil/worker/kafka/ImageDoneProducer.java
@@ -0,0 +1,30 @@
+package ru.daniil.worker.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.KafkaException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import ru.daniil.worker.dto.kafka.ImageDoneDto;
+import ru.daniil.worker.exceptions.InternalServerException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ImageDoneProducer {
+
+    private final KafkaTemplate<String, Object> transactionalTemplate;
+
+    @Value("${app.images.done.topic}")
+    private String topicDone;
+
+    public void sendMessage(ImageDoneDto message) {
+        log.info("Send message: {}", message);
+        try {
+            transactionalTemplate.executeInTransaction(it -> transactionalTemplate.send(topicDone, message));
+        } catch (KafkaException e) {
+            throw new InternalServerException("Kafka: " + e.getMessage());
+        }
+    }
+}
